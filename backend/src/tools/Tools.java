fa3802f82caa520e2ab98205966afdc4bf5401fc
Junita Sinambela <jujunale@gmail.com>
2016-02-19 14:42:33 +0700

    new class added

---
+++ backend/src/tools/Tools.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tools;
+
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author user
+ */
+public class Tools {
+    public static byte[] pngReader(String path){
+        File file = new File(path);
+
+        BufferedImage bufferedImage;
+        ByteOutputStream bos = null;
+        try {
+            bufferedImage = ImageIO.read(file);
+            bos = new ByteOutputStream();
+            ImageIO.write(bufferedImage, "png", bos);
+        } catch (IOException ex) {
+            Logger.getLogger(Tools.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                bos.close();
+            } catch (Exception e) {
+                
+            }
+        }
+        return bos == null ? null : bos.getBytes();
+    }
+    public static File pngWriter(String path, byte[] bytes){
+        
+    }
+}
